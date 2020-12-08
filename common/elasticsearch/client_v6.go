692142680b1a62b79dfb01e09a94196921faf20b
Saurabh Mistry <saurabh.mistry@cruxinformatics.com>
2020-12-07 22:30:41 -0800

    delete comments

--- common/elasticsearch/client_v6.go
+++ common/elasticsearch/client_v6.go
@@ -93,8 +93,6 @@ func NewV6Client(
 		elastic.SetURL(connectConfig.URL.String()),
 		elastic.SetRetrier(elastic.NewBackoffRetrier(elastic.NewExponentialBackoff(128*time.Millisecond, 513*time.Millisecond))),
 		elastic.SetDecoder(&elastic.NumberDecoder{}), // critical to ensure decode of int64 won't lose precise)
-		//elastic.SetSniff(false),
-		//elastic.SetHealthcheck(false),
 	)
 	if connectConfig.DisableSniff {
 		clientOptFuncs = append(clientOptFuncs, elastic.SetSniff(false))
